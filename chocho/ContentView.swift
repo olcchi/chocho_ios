e12014c1b721f55e80aeb7b249b5f220012f4a46
EKar <Earthb0y@outlook.com>
2026-05-22 11:49:24 +0800

    Initial Commit

---
+++ chocho/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chocho
+//
+//  Created by Ekar on 2026/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chocho/chochoApp.swift
@@ -0,0 +1,17 @@
+//
+//  chochoApp.swift
+//  chocho
+//
+//  Created by Ekar on 2026/5/22.
+//
+
+import SwiftUI
+
+@main
+struct chochoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
